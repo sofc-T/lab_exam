abd9cee39ca0fe6a37165fc20d75b6ca1c6590b4
Sofonyas Tizazu <sofstzz@gmail.com>
2024-12-30 09:38:47 +0300

    mysql db

---
+++ src/main/java/com/hr/hr/repository/LeaveManageNativeSqlRepo.java
@@ -0,0 +1,27 @@
+package com.hr.hr.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+
+import com.hr.hr.models.LeaveDetails;
+
+@Repository
+public class LeaveManageNativeSqlRepo {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @SuppressWarnings("unchecked")
+    public List<LeaveDetails> getAllLeavesOnStatus(String status) {
+        String sql = "SELECT * FROM leave_details WHERE status = :status";
+        Query query = entityManager.createNativeQuery(sql, LeaveDetails.class);
+        query.setParameter("status", status);  
+
+        return query.getResultList();
+    }
+}
