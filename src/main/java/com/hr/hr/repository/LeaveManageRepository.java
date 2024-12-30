09a0be7bcd5650b421e221e9a5e6f8c07d4b5f43
Sofonyas Tizazu <sofstzz@gmail.com>
2024-12-30 09:43:05 +0300

    leve management repo findONById

--- src/main/java/com/hr/hr/repository/LeaveManageRepository.java
+++ src/main/java/com/hr/hr/repository/LeaveManageRepository.java
@@ -4,23 +4,34 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.hr.hr.models.LeaveDetails;
+import com.hr.hr.models.UserInfo;
 
-@Repository(value = "leaveManageRepository")
-public interface LeaveManageRepository extends JpaRepository<LeaveDetails, Serializable> {
+@Repository(value = "userInfoRepository")
+public interface UserInfoRepository extends JpaRepository<UserInfo, Serializable> {
 
+    public UserInfo findByEmail(String email);
 
-    @Query(nativeQuery = true, value = "select array_to_json(array_agg(row_to_json(t))) from (select employee_name||' on leave' as title,to_char(from_date,'YYYY-MM-DD') as start,to_char(to_date,'YYYY-MM-DD') as end from leave_details) as t;")
-    public Object getAllLeavesAsJsonArray();
+    public List<UserInfo> findAllByOrderById();
 
-    @Query(nativeQuery = true, value = "select * from leave_details where active=true")
-    public List<LeaveDetails> getAllActiveLeaves();
+    public UserInfo findById(int id);
 
-    @Query(nativeQuery = true, value = "select * from leave_details where username=? order by id desc")
-    public List<LeaveDetails> getAllLeavesOfUser(String username);
+    @Transactional
+    @Modifying
+    @Query(value = "update userinfo set active=false where id=?", nativeQuery = true)
+    public void blockUser(int id);
 
+    @Transactional
+    @Modifying
+    @Query(value = "update userinfo set active=true where id=?", nativeQuery = true)
+    public void unBlockUser(int id);
 
+    @Transactional
+    @Modifying
+    @Query(value = "delete from userinfo where id=?", nativeQuery = true)
+    public void deleteUserById(int id);
 }
