373e8692a8e063c2e94ef9e03a71a7c2092680cf
Sofonyas Tizazu <sofstzz@gmail.com>
2024-12-31 09:56:58 +0300

    leave repo

--- src/main/java/com/hr/hr/repository/LeaveManageRepository.java
+++ src/main/java/com/hr/hr/repository/LeaveManageRepository.java
@@ -4,34 +4,23 @@ import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.hr.hr.models.UserInfo;
+import com.hr.hr.models.LeaveDetails;
 
-@Repository(value = "userInfoRepository")
-public interface UserInfoRepository extends JpaRepository<UserInfo, Serializable> {
+@Repository(value = "leaveManageRepository")
+public interface LeaveManageRepository extends JpaRepository<LeaveDetails, Serializable> {
 
-    public UserInfo findByEmail(String email);
+    // MySQL-compatible version
+    @Query(nativeQuery = true, value = "SELECT CONCAT(employee_name, ' on leave') AS title, DATE_FORMAT(from_date, '%Y-%m-%d') AS start, DATE_FORMAT(to_date, '%Y-%m-%d') AS end FROM leave_details")
+    public List<Object> getAllLeavesAsJsonArray();
 
-    public List<UserInfo> findAllByOrderById();
+    // Query for active leaves (MySQL-compatible)
+    @Query(nativeQuery = true, value = "SELECT * FROM leave_details WHERE active=true")
+    public List<com.hr.hr.models.LeaveDetails> getAllActiveLeaves();
 
-    public UserInfo findById(int id);
-
-    @Transactional
-    @Modifying
-    @Query(value = "UPDATE userinfo SET active=false WHERE id=:id", nativeQuery = true)
-    public void blockUser(int id);
-
-    @Transactional
-    @Modifying
-    @Query(value = "UPDATE userinfo SET active=true WHERE id=:id", nativeQuery = true)
-    public void unBlockUser(int id);
-
-    @Transactional
-    @Modifying
-    @Query(value = "DELETE FROM userinfo WHERE id=:id", nativeQuery = true)
-    public void deleteUserById(int id);
+    // Query for leaves of a specific user (MySQL-compatible)
+    @Query(nativeQuery = true, value = "SELECT * FROM leave_details WHERE username=? ORDER BY id DESC")
+    public List<com.hr.hr.models.LeaveDetails> getAllLeavesOfUser(String username);
 }
