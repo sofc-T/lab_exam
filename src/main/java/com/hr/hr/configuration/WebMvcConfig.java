c74b5936ba8c7f96efedc8de5bcf0bc6e89bf727
Sofonyas Tizazu <sofstzz@gmail.com>
2024-12-31 11:27:57 +0300

    packages and config

--- src/main/java/com/hr/hr/configuration/WebMvcConfig.java
+++ src/main/java/com/hr/hr/configuration/WebMvcConfig.java
@@ -3,14 +3,12 @@ package com.hr.hr.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
-	BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-	return bCryptPasswordEncoder;
+        return new BCryptPasswordEncoder();
     }
-
 }
