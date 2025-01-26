5f41b8b0321f3fe2bcf84ebe652bc4dce5578609
Sofonyas <sofstzz@gmail.com>
2025-01-26 09:41:00 +0300

    leave management nati

--- src/main/java/com/hr/hr/repository/LeaveManageNativeSqlRepo.java
+++ src/main/java/com/hr/hr/repository/LeaveManageNativeSqlRepo.java
@@ -2,20 +2,23 @@ package com.hr.hr.repository;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+// import javax.persistence.EntityManager;
+// import javax.persistence.PersistenceContext;
+// import javax.persistence.Query;
 
 import org.springframework.stereotype.Repository;
 
 import com.hr.hr.models.LeaveDetails;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 
 @Repository
 public class LeaveManageNativeSqlRepo {
 
     @PersistenceContext
     EntityManager entityManager;
-
     @SuppressWarnings("unchecked")
     public List<LeaveDetails> getAllLeavesOnStatus(String status) {
 
