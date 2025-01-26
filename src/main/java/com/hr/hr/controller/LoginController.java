8342a24b262555fb8e0e1df6b26a2157c87cbb38
Sofonyas <sofstzz@gmail.com>
2025-01-26 09:40:37 +0300

    login conroller

--- src/main/java/com/hr/hr/controller/LoginController.java
+++ src/main/java/com/hr/hr/controller/LoginController.java
@@ -1,9 +1,10 @@
 package com.hr.hr.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
-import org.json.JSONException;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -18,12 +19,12 @@ import com.hr.hr.models.UserInfo;
 import com.hr.hr.service.LeaveManageService;
 import com.hr.hr.service.UserInfoService;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 /**
- * This controller will provide the basic operations fo users. Like
- * signing-in,registering a new user.
+ * This controller will provide basic operations for users such as
+ * signing-in, registering a new user, and handling authentication.
  * 
- * @author navinkumark
- *
  */
 @Controller
 public class LoginController {
@@ -35,86 +36,91 @@ public class LoginController {
     LeaveManageService leaveManageService;
 
     /**
-     * This method opens up the login page if user is not authenticated
-     * otherwise redirects the user to user home page.
+     * This method opens up the login page if the user is not authenticated;
+     * otherwise, it redirects the user to the home page.
      * 
-     * @return
+     * @param mav ModelAndView object
+     * @return ModelAndView object
      */
-    @RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
-    public ModelAndView login(ModelAndView mav) {
-
-	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	UserInfo userInfo = userInfoService.findUserByEmail(auth.getName());
-
-	mav.addObject("userInfo", userInfo);
-	if (!(auth instanceof AnonymousAuthenticationToken)) {
-	    mav.setViewName("home");
-	    return mav;
-	}
-	mav.setViewName("login");
-	return mav;
+    @RequestMapping(value = {"/","/login"}, method = RequestMethod.GET)
+    public ModelAndView login(ModelAndView mav, HttpServletRequest request) {       
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        // System.out.println("Authentication Object: " + auth);
+        // If the user is authenticated, redirect to home
+        if (!(auth instanceof AnonymousAuthenticationToken)) {
+            UserInfo userInfo = userInfoService.findUserByEmail(auth.getName());
+            mav.addObject("userInfo", userInfo);
+            mav.setViewName("home");
+            return mav;
+        }
+        
+        // If not authenticated, show login page
+        mav.setViewName("login");
+        return mav;
     }
 
     /**
      * Opens the registration page to register a new user.
      * 
-     * @return ModelAndView
+     * @param mav ModelAndView object
+     * @return ModelAndView object
      */
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public ModelAndView registration(ModelAndView mav) {
-
-	UserInfo userInfo = new UserInfo();
-	mav.addObject("userInfo", userInfo);
-	mav.setViewName("registration");
-	return mav;
+        UserInfo userInfo = new UserInfo();
+        mav.addObject("userInfo", userInfo);
+        mav.setViewName("registration");
+        return mav;
     }
 
     /**
-     * Gets the form input from registration page and adds the user to the
+     * Gets the form input from the registration page and adds the user to the
      * database.
      * 
-     * @param user
-     * @param bindResult
-     * @return ModelAndView
+     * @param mav          ModelAndView object
+     * @param userInfo     User information object
+     * @param bindResult   Binding result for form validation
+     * @return ModelAndView object
      */
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public ModelAndView createNewUser(ModelAndView mav, @Valid UserInfo userInfo, BindingResult bindResult) {
 
-	UserInfo userExists = userInfoService.findUserByEmail(userInfo.getEmail());
+        UserInfo userExists = userInfoService.findUserByEmail(userInfo.getEmail());
 
-	if (userExists != null) {
-	    bindResult.rejectValue("email", "error.user", "User already exists with Email id");
-	}
+        if (userExists != null) {
+            bindResult.rejectValue("email", "error.user", "User already exists with this email ID");
+        }
 
-	if (bindResult.hasErrors()) {
-	    mav.setViewName("registration");
-	} else {
-	    userInfoService.saveUser(userInfo);
-	    mav.addObject("successMessage", "User registered successfully! Awaiting for Manager approval!!");
-	    mav.addObject("userInfo", new UserInfo());
-	    mav.setViewName("registration");
-	}
-	return mav;
+        if (bindResult.hasErrors()) {
+            mav.setViewName("registration");
+        } else {
+            userInfoService.saveUser(userInfo);
+            mav.addObject("successMessage", "User registered successfully! Awaiting Manager approval.");
+            mav.addObject("userInfo", new UserInfo());
+            mav.setViewName("login");
+        }
+        return mav;
     }
 
     /**
      * Shows the admin page after user authentication is done.
      * 
-     * @param request
-     * @return ModelAndView
-     * @throws JSONException
+     * @param mav ModelAndView object
+     * @param request HttpServletRequest
+     * @return ModelAndView object
      */
     @RequestMapping(value = "/user/home", method = RequestMethod.GET)
     public ModelAndView home(ModelAndView mav, HttpServletRequest request) throws Exception {
-
-	Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	UserInfo userInfo = userInfoService.findUserByEmail(auth.getName());
-	request.getSession().setAttribute("userInfo", userInfo);
-
-	mav.addObject("userInfo", userInfo);
-	mav.setViewName("home");
-	return mav;
-
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserInfo userInfo = userInfoService.findUserByEmail(auth.getName());
+        request.getSession().setAttribute("userInfo", userInfo);
+    
+        mav.addObject("userInfo", userInfo);
+        mav.setViewName("home");
+        return mav;
     }
+    
+
+    
 
 }
